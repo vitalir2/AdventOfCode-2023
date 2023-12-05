34bc10c542f1633c277f7925320475f1e06aeef8
vitalir2 <vitalyi100@mail.ru>
2023-12-05 09:32:14 +0300

    Day 4, part 2

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,6 +1,9 @@
 import kotlin.math.pow
 
 object Day04 : Challenge<Int>(day = 4) {
+
+    private val parseNumbersRegex = " +".toRegex()
+
     override fun solveFirstPart(input: Input): Int {
         return input
             .map(::parseCard)
@@ -8,15 +11,39 @@ object Day04 : Challenge<Int>(day = 4) {
     }
 
     override fun solveSecondPart(input: Input): Int {
-        TODO("Not yet implemented")
+        return input
+            .map(::parseCard)
+            .let(::processCards)
+    }
+
+    private fun processCards(cards: List<ScratchCard>): Int {
+        val cardIdToCopiesCount = mutableMapOf<Int, Int>()
+        for (card in cards) {
+            cardIdToCopiesCount[card.id] = 1
+        }
+
+        for (card in cards) {
+            repeat(card.numberOfWins) { offset ->
+                cardIdToCopiesCount.computeIfPresent(card.id + offset + 1) { _, oldCount ->
+                    oldCount + cardIdToCopiesCount.getValue(card.id)
+                }
+            }
+        }
+
+        return cardIdToCopiesCount.map { it.value }.sum()
     }
 
     private fun parseCard(string: String): ScratchCard {
         val (winningNumbersString, numbersYouHaveString) = string
             .substringAfter(": ").split(" | ")
+        val cardId = string
+            .substringBefore(": ")
+            .takeLastWhile(Char::isDigit)
+            .toInt()
         val winningNumbers = parseNumbers(winningNumbersString).toSet()
         val numbersYouHave = parseNumbers(numbersYouHaveString)
         return ScratchCard(
+            id = cardId,
             winningNumbers = winningNumbers,
             numbers = numbersYouHave,
         )
@@ -31,26 +58,27 @@ object Day04 : Challenge<Int>(day = 4) {
     }
 }
 
-private val parseNumbersRegex = " +".toRegex()
-
 class ScratchCard(
+    val id: Int,
     val winningNumbers: Set<Int>,
     val numbers: List<Int>,
 ) {
 
-    val points: Int
-        get() {
-            val numberOfWinningNumbers = numbers.count(::isNumberWinning)
-            return if (numberOfWinningNumbers > 0) {
-                2f.pow(numberOfWinningNumbers - 1).toInt()
-            } else {
-                0
-            }
-        }
+    val numberOfWins: Int = numbers.count(::isNumberWinning)
 
     private fun isNumberWinning(number: Int): Boolean {
         return number in winningNumbers
     }
 }
 
-fun main() = Day04.part1()
+private val ScratchCard.points: Int
+    get() {
+        val numberOfWins = numberOfWins
+        return if (numberOfWins > 0) {
+            2f.pow(numberOfWins - 1).toInt()
+        } else {
+            0
+        }
+    }
+
+fun main() = Day04.executeParts()
--- src/test/kotlin/Day04Test.kt
+++ src/test/kotlin/Day04Test.kt
@@ -60,5 +60,66 @@ class Day04Test : ChallengeTest<Int>(day = Day04) {
                 part1(input) shouldBeExactly 13
             }
         }
+
+        "part 2" - {
+            "only one card with no copies" {
+                val input = """
+                    Card 1: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+                """.trimIndent()
+
+                part2(input) shouldBeExactly 1
+            }
+
+            "two cards with no copies" {
+                val input = """
+                    Card 1: 41 47 82 86 18 | 83 85  6 31 17  9 48 53
+                    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                """.trimIndent()
+
+                part2(input) shouldBeExactly 2
+            }
+
+            "a first card creates copy of the second one" {
+                val input = """
+                    Card 1: 41 48 83 86 17 | 83 87  6 31 19  9 42 53
+                    Card 2: 13 31 20 16 59 | 61 30 68 82 17 32 24 19
+                """.trimIndent()
+
+                part2(input) shouldBeExactly 3
+            }
+
+            "a first card creates many copies of next ones" {
+                val input = """
+                    Card 1:  9 63 | 63  9  9
+                    Card 2:  1  2 |  9 55 91
+                    Card 3: 84 12 |  1 15 32
+                """.trimIndent()
+
+                part2(input) shouldBeExactly 5
+            }
+
+            "a card with copies creates new copies from the all card instances" {
+                val input = """
+                    Card 1:  9 63 | 63  9  9
+                    Card 2: 55  2 |  9 55 91
+                    Card 3: 84 12 |  1 15 32
+                """.trimIndent()
+
+                part2(input) shouldBeExactly 1 + 2 + 4
+            }
+
+            "part example" {
+                val input = """
+                    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+                """.trimIndent()
+
+                part2(input) shouldBeExactly 30
+            }
+        }
     }
 }
